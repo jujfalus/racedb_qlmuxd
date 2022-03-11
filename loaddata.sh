08e6dbdd12966a305de125e07b987a382eb09266
jujfalus <66374147+jujfalus@users.noreply.github.com>
2022-03-10 20:27:14 -0800

    Add files via upload

--- loaddata.sh
+++ loaddata.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-cat $1 | docker exec -i racedb_8080_app python3 /RaceDB/manage.py loaddata --format=json -
+cat $1 | docker exec -i racedb_8080_primary python3 /RaceDB/manage.py loaddata --format=json -
